779c23f3ed7a70d1e9201f6c4e46c2015eba6994
Rabhynoide <remi.duc@hotmail.fr>
2017-12-17 20:26:05 +0100

    curl

--- testcurl.php
+++ testcurl.php
@@ -1,6 +1,14 @@
 <?php
-$curl = curl_init('https://esi.tech.ccp.is/latest/markets/10000002/history/?datasource=tranquility&type_id=3687');
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => 'https://esi.tech.ccp.is/latest/markets/10000002/history/?datasource=tranquility&type_id=3687',
+));
+
 $result = curl_exec($curl);
+
 echo $result;
-curl_close();
+
+curl_close($curl);
 ?>
