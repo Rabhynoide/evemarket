76a6becc956003fc34d6275dedfbd1c12ae1eb33
Rabhynoide <remi.duc@hotmail.fr>
2017-12-17 20:22:20 +0100

    a

--- testcurl.php
+++ testcurl.php
@@ -1,10 +1,6 @@
 <?php
-
-$curl = curl_init('http://testcURL.com');
+$curl = curl_init('https://esi.tech.ccp.is/latest/markets/10000002/history/?datasource=tranquility&type_id=3687');
 $result = curl_exec($curl);
-if(!curl_exec($curl)){
-    die('Error: "' . curl_error($curl) . '" - Code: ' . curl_errno($curl));
-}
+echo $result;
 curl_close();
-echo 'test';
 ?>
